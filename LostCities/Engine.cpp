72beb52d1215fbca0399fc2f108057ffb068a906
EphesosX <tkt5@njit.edu>
2016-09-18 13:21:58 -0700

    reformatted

--- LostCities/Engine.cpp
+++ LostCities/Engine.cpp
@@ -19,9 +19,62 @@ Deck::Deck() {
 	shuffle();
 }
 
+void Deck::shuffle() {
+	// initialize random seed
+	srand(time(NULL));
+	// swap the last element with a random element(in place swap possible)
+	// fix it and swap the second to last with an element below it
+	for (size_t i = cards.size() - 1; i >0; i--) {
+		size_t j = rand() % (i + 1); // position to switch w/ position i
+		// j is always <= i, so positions > i are fixed
+		iter_swap(cards.begin() + j, cards.begin() + i); // swap the cards
+	}
+}
+
+void Board::addCard(Card* card) {
+	if (canAddCard(card)) {
+		cards[card->color].push_back(card);
+	}
+}
+
+int PlayerBoard::getScore() {
+	int score = 0;
+	for (int i = 0; i < 5; i++) {
+		if (cards[i].size() >= 0) {
+			int nHandshakes = 0;
+			int subtotal = 0;
+			for (Card* c : cards[i]) {
+				if (c->getValue() == 0) {
+					nHandshakes += 1;
+				}
+				subtotal += c->getValue();
+			}
+			score += (nHandshakes + 1) * (subtotal - 20);
+			if (cards[i].size() >= 8) {
+				score += 20;
+			}
+		}
+	}
+}
+
+vector<Card*> DiscardBoard::getTopDiscards() {
+	vector<Card*> tops;
+	for (int i = 0; i < 5; i++) {
+		if (cards[i].size() > 0) {
+			tops[i] = cards[i].back();
+		}
+	}
+	return tops;
+}
+
+void DiscardBoard::pickupDiscard(int color, vector<Card*>* hand) {
+	if (cards[color].size() > 0) {
+		hand->push_back(cards[color].back());
+		cards[color].pop_back();
+	}
+}
+
 int main() {
-	
-	
 	//initialize game
 	//create deck
 	Deck deck;
--- LostCities/Engine.h
+++ LostCities/Engine.h
@@ -21,34 +21,20 @@ public:
 		number = new_number;
 	}
 	// Handshakes are encoded as 0
-	bool isHandshake() {
-		return number == 0;
-	}
+	bool isHandshake() {return number == 0;}
 
-	int getValue() {
-		return number;
-	}
+	int getValue() {return number;}
 };
 
 class Deck {
 	vector<Card*> cards;
 
 public:
-	//does the stuff that implementation has in it.
+	// Creates a default Lost Cities deck
 	Deck();
 
 	// randomly permute the cards
-	void shuffle() {
-		// initialize random seed
-		srand(time(NULL));
-		// swap the last element with a random element(in place swap possible)
-		// fix it and swap the second to last with an element below it
-		for (size_t i = cards.size()-1; i >0; i--) {
-			size_t j = rand() % (i+1); // position to switch w/ position i
-			// j is always <= i, so positions > i are fixed
-			iter_swap(cards.begin() + j, cards.begin() + i); // swap the cards
-		}
-	}
+	void shuffle();
 	
 	// Draw a card from the top of the deck and add it to a player's hand
 	void draw(vector<Card*>* hand) {
@@ -57,9 +43,7 @@ public:
 	}
 
 	// get the array of cards
-	vector<Card*> getCards() {
-		return cards;
-	}
+	vector<Card*> getCards() {return cards;}
 };
 
 // An array of cards, separated into 5 colors
@@ -68,19 +52,11 @@ protected:
 	array<vector<Card*>, 5> cards;
 
 public:
-	Board() {
+	Board();
 
-	}
+	void addCard(Card* card);
 
-	void addCard(Card* card) {
-		if (canAddCard(card)) {
-			cards[card->color].push_back(card);
-		}
-	}
-
-	bool canAddCard(Card* card) {
-		return true;
-	}
+	bool canAddCard(Card* card) {return true;}
 };
 
 // Allows for scoring and restricts cards added
@@ -89,48 +65,17 @@ class PlayerBoard : Board {
 		return cards[card->color].back()->getValue() <= card->getValue();
 	}
 
-	int getScore() {
-		int score = 0;
-		for (int i = 0; i < 5; i++) {
-			if (cards[i].size() >= 0) {
-				int nHandshakes = 0;
-				int subtotal = 0;
-				for (Card* c : cards[i]) {
-					if (c->getValue() == 0) {
-						nHandshakes += 1;
-					}
-					subtotal += c->getValue();
-				}
-				score += (nHandshakes + 1) * (subtotal - 20);
-				if (cards[i].size() >= 8) {
-					score += 20;
-				}
-			}
-		}
-	}
+	int getScore();
 };
 
 // Allows the top card to be removed
 class DiscardBoard : Board{
 	// Retrieve pointers to the top card of each discard stack
-	vector<Card*> getTopDiscards() {
-		vector<Card*> tops;
-		for (int i = 0; i < 5; i++) {
-			if (cards[i].size() > 0) {
-				tops[i] = cards[i].back();
-			}
-		}
-		return tops;
-	}
+	vector<Card*> getTopDiscards();
 
 	// Remove the top card of a discard pile and add it
 	// to a player's hand
-	void pickupDiscard(int color, vector<Card*>* hand) {
-		if (cards[color].size() > 0) {
-			hand->push_back(cards[color].back());
-			cards[color].pop_back();
-		}
-	}
+	void pickupDiscard(int color, vector<Card*>* hand);
 };
 
 class Player {
